89241dd753128d7f591a229399ecf2c780647c07
Savita Rao <savitarao702@gmail.com>
2025-02-01 18:01:27 +0530

    some changes in public file

--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -5,7 +5,7 @@ export const Public = () => {
             <div className='publicDiv'>
                 <h1>Let's make things happen</h1>
                 <p>Contact us today to learn more about how our digital    <br /> makerting services can help your business grow and <br /> succeed online</p>
-                <button>Get started..</button>
+                <button>Get started</button>
             </div>
             <div className='publicImgDiv'>
                 <img src="public.png" alt="A public image" />
