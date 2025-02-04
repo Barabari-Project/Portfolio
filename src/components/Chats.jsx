f69158d524207d2570570c45610e168a2824de80
Savita Rao <savitarao702@gmail.com>
2025-02-04 10:05:54 +0530

    some changes in chats file

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,59 +1,48 @@
 import React, { useState } from "react";
 import "../styling/Chats.css";
+// const [Popup, setPopup] = useState(false)
 
 export const Chats = () => {
-  const [showChats1, setShowChats1] = useState(false);
-
-  // Function to handle button click
-  const handleChatClick = () => {
-    setChats1(true);
-  };    
-
-  // Function to go back to the main page
-  const handleBackClick = () => {
-    setShowChats1(false);
-  };
-
+  const onClickChat = () => {
+    const chatpage = document.querySelector(".chatPage");
+    chatpage.style.display = "block"
+    // chatpage.style.visibilty = "visible"
+    // .style.opacity = 0.5;
+  }
   return (
     <div className="contactDiv">
-      {!showChats1 ? (
-        <div>
-          <div className="logoDiv">
-            <img src="logo.png" alt="Barabari logo" />
-            <p>The Barabari Project</p>
-            <div className="contactButton">
-              <button>Contact</button>
-              <svg class="icon" viewBox="0 0 100 80" width="46" height="20">
-                <rect width="100" height="15"></rect>
-                <rect y="35" width="100" height="15"></rect>
-                <rect y="65" width="100" height="15"></rect>
-              </svg>
+      <div>
+        <div className="logoDiv">
+          <img src="logo.png" alt="Barabari logo" />
+          <p>The Barabari Project</p>
+          <div className="contactButton">
+            <button>Contact</button>
+            <svg className="icon" viewBox="0 0 100 80" width="46" height="20">
+              <rect width="100" height="15"></rect>
+              <rect y="35" width="100" height="15"></rect>
+              <rect y="65" width="100" height="15"></rect>
+            </svg>
 
-            </div>
-          </div>
-          <div className="publicImgDiv">
-            <div className="logoDivPara">
-              <h1>
-                Let our students <br /> build your <span>ChatBots</span>
-              </h1>
-              <p>
-                We are Section 8 Non-Profit upskilling candidates <br />
-                from low-income families democratizing access <br />
-                to tech design employment opportunities
-              </p>
-              <button>Hire from us</button>
-            </div>
-            <div>
-              <img className="publicImg" src="public.png" alt="A public Image" />
-              <button className="chatButton" onClick={handleChatClick}>
-                Chat with us
-              </button>
-            </div>
           </div>
         </div>
-      ) : (
-        <setShowChats1 onBack={handleBackClick} /> // Using the ChatPage component
-      )}
+        <div className="publicImgDiv">
+          <div className="logoDivPara">
+            <h1>
+              Let our students <br /> build your <span>ChatBots</span>
+            </h1>
+            <p>
+              We are Section 8 Non-Profit upskilling candidates <br />
+              from low-income families democratizing access <br />
+              to tech design employment opportunities
+            </p>
+            <button>Hire from us</button>
+          </div>
+          <div>
+            <img className="publicImg" src="public.png" alt="A public Image" />
+            <button className="chatButton" onClick={onClickChat}>Chat with us</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Chats1.jsx
+++ src/components/Chats1.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
 import "../styling/Chats1.css"
-export const Chats1 = ({ onBack }) => {
-    const [Chats2, setChats2] = useState(false);
 
+export const Chats1 = () => {
+
+  const onClickStartChat = () => {
+    const chatcard = document.querySelector(".chat-card");
+    chatcard.style.display = "block"
+    // chatpage.style.visibilty = "visible"
+    // .style.opacity = 0.5;
+  }
 
       // Function to go back to the main page
   const handleBackClick = () => {
-    setChats2 (false);
+    setChats2(false);
   }
 
     return (
       <div className="chatPage">
-        <button className="backButton" onClick={onBack}>
-        </button>
+        <button className="backButton"></button>
         <div className="chatContainer">
           <div className="backChat">
-            <div onClick={onBack}>|-</div>
+            <div>|-</div>
             <div>Chat with us</div>
-            <div onClick={onBack}>x</div>
+            <div>x</div>
           </div>
           <img className="searchImg" src="search.png" alt="" />
           <h1>Hi There!</h1>
@@ -26,7 +31,7 @@ export const Chats1 = ({ onBack }) => {
             We are happy to discuss any potential projects. If you have a couple
             of minutes, we would love to hear more.
           </p>
-          <button className="startChatButton">Start Chat</button>
+          <button className="startChatButton" onClick={onClickStartChat}>Start Chat</button>
           <div className="startChatButtonPara">
             <h2>Got any other queries?</h2>
             <p>Please drop us an email at </p>
